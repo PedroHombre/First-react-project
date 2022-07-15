6837568a205b797c5a77bf201dd7bdd6dfb22b98
PedroHombre <radekgornackiv2@gmail.com>
2022-07-15 09:47:43 +0200

    base component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>First react component</h1>
+				<p>App base</p>
+			</div>
+		);
+	}
+}
+
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Component file
+import TodoContainer from './components/TodoContainer';
+
 const element = <h1>First h1 element</h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
