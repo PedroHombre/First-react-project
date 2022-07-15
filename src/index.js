9894e9dbfae8247708c791eea81a9d757a112c1e
PedroHombre <radekgornackiv2@gmail.com>
2022-07-15 09:54:38 +0200

    Toggled strict mode

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 // Component file
 import TodoContainer from './components/TodoContainer';
 
-const element = <h1>First h1 element</h1>;
-
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
+ReactDOM.render(
+	<React.StrictMode>
+		<TodoContainer />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
